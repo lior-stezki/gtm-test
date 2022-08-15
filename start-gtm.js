42c17a0a52a4aaf5062fa2a01fd874cd716d594e
lior-stezki <liorstezki@gmail.com>
2022-08-15 12:56:42 +0300

    Update start-gtm.js

--- start-gtm.js
+++ start-gtm.js
@@ -1,4 +1,6 @@
-  var s = document.getElementsByTagName('script')[0];
+<script>
+var s = document.getElementsByTagName('script')[0];
 var _s = document.createElement('script');
 _s.src='https://gtm-test.free.beeceptor.com/file?cb=' + Math.floor(Math.random()*1e6);
 s.parentNode.insertBefore(_s, s);
+</script>
